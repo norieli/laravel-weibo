d61458ab089659a43b92716e7bc13114436c1b10
norie <norieli@qq.com>
2021-08-16 16:37:23 +0800

    email找回重置密码

--- app/Models/User.php
+++ app/Models/User.php
@@ -38,6 +38,15 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
+    public static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($user) {
+            $user->activation_token = str_random(30);
+        });
+    }
+
     /**
      * avater
      *
